93a93799fd36060775ae6fe2339eaa7a6279a80d
AdrianV2022 <146434555+DKchonkykong@users.noreply.github.com>
2024-07-07 21:19:37 +0100

    here as well

---
+++ Capstone v2/Capstone v2/FoodItemActions.cs
@@ -0,0 +1,7 @@
+//useless class atm it's supposed to have all the logic for the class
+namespace Capstone_v2;
+
+public class FoodItemActions
+{
+    
+}
---
+++ Capstone v2/Capstone v2/MenuItem.cs
@@ -0,0 +1,10 @@
+//not working atm but it's supposed to hold the main menu logic
+namespace Capstone_v2;
+
+public class MenuItem
+{
+    //enter customer name
+    //console.readLine
+    //enter adress
+    //console.readLine etc.
+}
