0934b65966e482b062104e36ec21ba8daac793a1
AdrianV2022 <146434555+DKchonkykong@users.noreply.github.com>
2024-07-12 18:29:40 +0100

    extra class about menuItem i forgot

---
+++ Capstone v2/Capstone v2/MenuItem.cs
@@ -0,0 +1,40 @@
+namespace Capstone_v2;
+//this is a class that is supposed to be a sub menu 
+public class MenuItem
+{
+    //psuedo code
+    //unsure if need to have this: could be needed for adding specific values
+    //but i don't think i need this 
+    // public void RunAction(object[] parameters)
+    //{
+        //_action.DynamicInvoke(parameters);
+    //}
+    
+    
+    //constructor and stuff here 
+    //private FoodManager _manager;
+
+    //public MainMenu(FoodManager manager)
+    //{
+        //_manager = manager;
+    //}
+    
+    //    public override void CreateMenu()
+    //this is how it will most likely look for displaying menus pretty similar to how i have done it before
+    //{
+        //_menuItems.Clear();
+        //_menuItems.Add(new AddNewFoodMenuItem(_manager));
+        //_menuItems.Add(new AddNewOrderMenuItem(_manager));
+        //_menuItems.Add(new DisplayAllOrderMenu(_manager.OrderMenus));
+        //_menuItems.Add(new DisplayAllFoodMenuItem(_manager.Foods));
+        //_menuItems.Add(new DisplayRecepiesCopiesMenuItem(_manager.Copy));
+       // _menuItems.Add(new AddRecepieCopyConsoleMenu(_manager));
+     //   _menuItems.Add(new ExitMenuItem(this));
+   // }
+    //public override string MenuText()
+    //{
+   //     return _manager.Name;
+ //   }
+//}
+
+}
